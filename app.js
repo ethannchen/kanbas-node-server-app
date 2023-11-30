60fe2e95c2e96345a70628ad9cdbd5d5e2f9a316
yy <yichen@YY-MacBook-Air.local>
2023-11-30 08:48:27 -0500

    a6

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
@@ -16,6 +24,21 @@ app.use(
 );
 app.use(express.json());
 
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+UserRoutes(app);
 Hello(app);
 Lab5(app);
 CourseRoutes(app);
